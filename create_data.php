75510cd9349565b374aaa44469a5913dd9a2abf4
Caleb Wallace <cwallace02cs@gmail.com>
2024-09-17 12:45:58 -0400

    Initial Commit

---
+++ create_data.php
@@ -0,0 +1,145 @@
+<?php
+$firstNamesFile = fopen("data/first_names.csv", "r");
+$firstNames = fgetcsv($firstNamesFile, 0, ",");
+//Open File -> Separate each name into an index in an array
+
+$lastNames = file("data/last_names.txt");
+//Goes line by line luckily. Which results in an array
+
+$streetNamesFile = file("data/street_names.txt");
+$allStreetNames = [];
+//Open File and initialize an empty array
+
+foreach ($streetNamesFile as $names) {
+    $lineNames = explode(":", $names);
+    $trimmedNames = array_map("trim", $lineNames);
+    $allStreetNames = array_merge($allStreetNames, $trimmedNames);
+    //for each line from the array returned from file() do this:
+    //every line separate the words into an array
+    //trim any white space
+    //merge the final trimmed array with the allStreetNames Array
+}
+
+$streetTypesFile = file("data/street_types.txt");
+$allStreetTypes = preg_split('/\.\.;/', $streetTypesFile[0]);
+//Street types are all on one line
+//use a regular expression and preg_split to look for a pattern of ..;
+//seperate words based off that criteria 
+
+$domainNamesFile = file("data/domains.txt");
+$allDomains = preg_split('/(?<=\.com|\.edu)\./', $domainNamesFile[0]);
+//domains are all one one line so again we can use a regular expression to get domains
+//look back for a .com or edu. then look for a period following this 
+function generateCustomerData()
+{
+    global $firstNames, $lastNames, $allStreetNames, $allStreetTypes, $allDomains;
+
+    $randomCustomerData = array();
+
+    for ($i = 0; $i < 25; $i++) {
+        $currentLine = array($i, $firstNames[rand(0, count($firstNames) - 1)], $lastNames[rand(0, count($lastNames) - 1)], rand(10, 999) . " " . $allStreetNames[rand(0, count($allStreetNames) - 1)] . " " . $allStreetTypes[rand(0, count($allStreetTypes) - 1)]);
+        $fullEmail = $currentLine[1] . "." . trim($currentLine[2] . " ") . "@" . $allDomains[rand(0, count($allDomains) - 1)];
+        array_push($currentLine, $fullEmail);
+        array_push($randomCustomerData, $currentLine);
+    }
+    return $randomCustomerData;
+}
+function displayCustomerData($data_Arr)
+{
+    for ($i = 0; $i < count($data_Arr); $i++) {
+        print "<tr>";
+        for ($j = 0; $j < count($data_Arr[$i]); $j++) {
+            $currValue = $data_Arr[$i][$j];
+            print ("<td>$currValue</td>");
+        }
+        print "</tr>";
+    }
+}
+
+function writeDataToFile($data_Arr)
+{
+    $dataFile = fopen("data/customers.txt", "a");
+
+    for ($i = 0; $i < count($data_Arr); $i++) {
+
+        $trimmedValues = array_map('trim', $data_Arr[$i]);
+        $currLineValues = implode(":", $trimmedValues);
+        fwrite($dataFile, $currLineValues . "\n");
+    }
+
+    fclose($dataFile);
+
+}
+?>
+
+<h1
+    style="text-align: center; margin-top: 10px; background-color:black; color: white; padding:10px; border-radius: 5px">
+    Files Assignment
+</h1>
+<div class="container" style="display:flex; justify-content:space-evenly; border: 2px solid black;">
+
+    <div class="container" style="margin-top: 10px;">
+        <h3>First Names Array</h3>
+        <pre style="border: 1px solid black; padding: 5px;"><?php
+        print_r($firstNames);
+        ?></pre>
+    </div>
+    <div class="container">
+        <h3>Last Names Array</h3>
+        <pre style="border: 1px solid black; padding: 5px; "><?php
+        print_r($lastNames);
+
+        ?></pre>
+    </div>
+    <div class="container">
+        <h3>Street Names Array</h3>
+        <pre style="border: 1px solid black; padding: 5px;"><?php
+
+        print_r($allStreetNames);
+        ?></pre>
+    </div>
+    <div class="container">
+        <h3>Street Types Array</h3>
+        <pre style="border: 1px solid black; padding: 5px;"><?php
+        print_r($allStreetTypes);
+
+        ?></pre>
+    </div>
+    <div class="container">
+        <h3>Email Domain Array</h3>
+        <pre style="border: 1px solid black; padding: 5px;"><?php
+        print_r($allDomains);
+
+        ?></pre>
+
+    </div>
+
+</div>
+<h2
+    style="text-align: center; margin-top: 10px; background-color:black; color: white; padding:10px; border-radius: 5px">
+    Random Customer Data Table</h2>
+<div class="container"
+    style="display:flex; justify-content: center; border: 2px solid black; padding: 25px; margin: 5px">
+
+    <table border="3">
+        <tr>
+            <th>Customer ID</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Address</th>
+            <th>Email</th>
+        </tr>
+        <?php
+        $tableData = generateCustomerData();
+        displayCustomerData($tableData);
+        writeDataToFile($tableData);
+        ?>
+    </table>
+    <div class="container" style="display: flex; justify-content:center">
+
+    </div>
+</div>
+
+<?php
+
+?>
